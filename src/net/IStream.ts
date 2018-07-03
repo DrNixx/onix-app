3a0c23aa9d817fbd97a63cc63ef7f5f08818c11d
Nick Panteleeff <drnix@chess-online.com>
2018-07-03 19:47:07 +0300

    Temporary use rxjs-compat

--- src/net/IStream.ts
+++ src/net/IStream.ts
@@ -1,4 +1,6 @@
-import { Subscription, BehaviorSubject } from "rxjs";
+//import { Subscription, BehaviorSubject } from "rxjs";
+import { Subscription } from "rxjs/Subscription";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { IStreamMessage } from "./IStreamMessage";
 import { ConnectionStatus } from "./ConnectionStatus";
 

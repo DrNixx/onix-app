68b6a92b08192280b6867fd751b252c1c275b2ef
DrNixx <drnixx@gmail.com>
2020-12-03 03:21:14 +0300

    Extend user interfaces

--- src/types/ISiteUser.ts
+++ src/types/ISiteUser.ts
@@ -1,22 +1,25 @@
 export interface IUserProfile {
-    country?: string
+    country?: string;
 }
 
 export interface IChessTitle {
-    id: string,
-    name: string
+    id: string;
+    name: string;
 }
 
-export interface ISiteUser {
-    id?: number,
-    name: string,
-    display?: string,
-    aurl?: string,
-    online?: string|boolean,
-    language?: string,
-    patron?: string,
-    status?: string,
-    icon?: string,
-    profile?: IUserProfile,
-    title?: string | IChessTitle,
+export interface IChessUser {
+    id?: number|string;
+    name: string;
+    display?: string;
+    aurl?: string;
+    profile?: IUserProfile;
+    title?: string | IChessTitle;
+}
+
+export interface ISiteUser extends IChessUser {
+    online?: string|boolean;
+    language?: string;
+    patron?: string;
+    status?: string;
+    icon?: string;
 }
